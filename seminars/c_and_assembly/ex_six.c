832bcfc250d9a72e59aef06cc8a9974db2a9a690
Sebastian Porling <sebastianporling@n145-p181.eduroam.kth.se>
2018-01-23 10:45:45 +0100

    Made code cleaner

--- seminars/c_and_assembly/ex_six.c
+++ seminars/c_and_assembly/ex_six.c
@@ -3,15 +3,13 @@
 // Squares every element from the in array 
 // and reverses the output in out array
 void square_reverse(const double *x, double *y, const int len){
-    for(int i = 0; i < len; i++){
+    for(int i = 0; i < len; i++)
         *(y+len-i-1) = *(x+i) * *(x+i);
-    }
 }
 
 // Declares in and out array length of 3,
 // calls the square reverse and prints the result.
 int main(){
-
     double in[] = {11.0, 20.0, 100.0}; 
     double out[3];
     int len = sizeof(out)/sizeof(double);
